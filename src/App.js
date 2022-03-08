60d0b92c837c9c9655a4e2712d7dff9d5797e8c1
sam1322 <sriram.pant5@gmail.com>
2022-03-08 20:53:44 +0530

    Completed it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React , {useState ,useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
- return (<h1>Hello</h1>)
+  const [advice, setAdvice] = useState("")
+
+  const fetchAdvice = () =>{
+    axios.get('https://api.adviceslip.com/advice')
+      .then((resp)=>{
+        const {advice} = resp.data.slip;
+        console.log(advice)
+        setAdvice(advice)
+      })
+      .catch(console.log)
+  }
+
+  useEffect(fetchAdvice,[])
+
+ return (
+      <div className='app'>
+        <div className='card'>
+          <h1 className='heading'>{advice}</h1>
+          <button className='button' onClick={fetchAdvice}>
+           {/* Give me advice */}
+           <span>Give me advice</span>
+          </button>
+        </div>
+      </div>
+  )
 }
 
 export default App;
